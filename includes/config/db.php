bdc04e92e9ab4b6389af77342b4875d1f447fa67
shivanisable13 <shivanisable075@gmail.com>
2026-05-09 02:31:47 +0530

    Update db.php

--- includes/config/db.php
+++ includes/config/db.php
@@ -17,6 +17,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->exec("SET time_zone = '+05:30'");
 
 } catch (PDOException $e) {
 
