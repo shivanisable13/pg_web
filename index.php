79b9055098111262d5f2fa1136de28660d8afe6b
shivanisable13 <shivanisable075@gmail.com>
2026-05-09 11:52:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ $featuredPGs = $stmt->fetchAll();
             <div class="col-lg-6 text-center fade-in">
 
                 <img 
-                    src="https://www.magicbricks.com/paying-guest-for-boys-in-belgaum-pppfr"
+                    src="https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?q=80&w=600&auto=format&fit=crop"
                     class="img-fluid rounded-5 shadow-lg"
                     alt="Luxury PG"
                     style="max-height:650px; object-fit:cover;"
